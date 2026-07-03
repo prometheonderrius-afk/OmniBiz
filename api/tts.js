3d02c1dd430caf44262499da957251b568eaecdd
dan <prometheonderrius@gmail.com>
2026-07-03 03:32:19 -0400

    Update voice to Schedar

--- api/tts.js
+++ api/tts.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
           speechConfig: {
             voiceConfig: {
               prebuiltVoiceConfig: {
-                voiceName: "Puck"
+                voiceName: "Schedar"
               }
             }
           }
